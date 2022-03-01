60c3c8a3a1e8e6fdf3730a83eed39e42240248d7
Caligo <schattenetcaligo@gmail.com>
2022-03-01 16:49:30 +0530

    main file

---
+++ maid.js
@@ -0,0 +1,59 @@
+import Client from "./structures/Client.js";
+import { Discord } from "./config.js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import fs from 'fs'
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const client = new Client(Discord, {
+  commandPrefix: "maid",
+  master: "254950404050255872",
+  unknownCommandResponse: false,
+  intents: [
+    "guildMembers",
+    "guilds",
+    "guildMessages",
+    "guildMessageReactions",
+    "directMessages",
+    "directMessageReactions",
+  ],
+  allowedMentions: {
+    everyone: false,
+    roles: false,
+    users: true,
+    repliedUser: true,
+  },
+  messageLimit: 10,
+  restMode: true,
+  defaultImageSize: 1024,
+  disableEvents: [
+    "CHANNEL_CREATE",
+    "CHANNEL_UPDATE",
+    "CHANNEL_DELETE",
+    "GUILD_BAN_ADD",
+    "GUILD_BAN_REMOVE",
+    "GUILD_ROLE_CREATE",
+    "GUILD_ROLE_DELETE",
+    "GUILD_ROLE_UPDATE",
+    "MESSAGE_DELETE_BULK",
+  ],
+});
+
+const eventFiles = fs.readdirSync(__dirname + "/events/");
+
+for (const file of eventFiles) {
+  const eventHandler = await import(`./events/${file}`);
+  const eventName = file.split(".")[0];
+  console.log(eventName)
+  console.log(eventHandler.default)
+  client.on(eventName, (...args) => eventHandler.default(client, ...args));
+}
+
+await client.registry.registerCommands(__dirname + "/commands/");
+console.log(client.registry.commands.map((x) => x.name));
+console.log(client.registry.commands.size);
+//console.log(await client.registry.updateCommands(client.registry.commands.map(x => x)))
+//console.log(JSON.stringify(await client.registry.commands.get("kuro").update()))
+//console.log(await client.registry.commands.get("gif").json())
+// setTimeout(() => console.log(client.medea.abilities.cache.map(x => x.ID)), 3000)
+client.connect();
